a94e8e61a02066ab9bd99980ff8d03d78202973c
gauravbole <gaurav.r.bole@gmail.com>
2024-12-20 16:01:01 +0530

    basic priority based structure

--- src/sandeshah/notification/base.py
+++ src/sandeshah/notification/base.py
@@ -3,6 +3,7 @@ import time
 from config import Priority
 from typing import Dict
 
+
 class Notification:
     def __init__(self, message_template: str, priority: Priority, channel: str,
                  recipient: str, template_data: Dict = None):
